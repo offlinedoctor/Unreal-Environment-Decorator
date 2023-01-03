d83d02b422868b8252b084e4d0349a9c163abfc6
tng <37785333+offlinedoctor@users.noreply.github.com>
2023-01-04 01:20:11 +1100

    Convert to a plugin.

--- Source/EditorUtilityPlugin/EditorUtilityPlugin.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EditorUtilityPlugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EditorUtilityPlugin, "EditorUtilityPlugin" );
